e151a2b9b6b4622e793c6b04783cdc380f19b17a
hanjg <409017803@qq.com>
2018-03-19 21:48:13 +0800

    init
    Signed-off-by: hanjg <409017803@qq.com>

---
+++ src/Generator.java
@@ -0,0 +1,41 @@
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class Generator {
+
+    public void generator() throws Exception {
+
+        List<String> warnings = new ArrayList<String>();
+        //允许覆盖
+        boolean overwrite = true;
+        //配置文件
+        File configFile = new File("generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Generator generator = new Generator();
+            generator.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
